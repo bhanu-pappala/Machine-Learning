ebddf578f4a26018f0ccd7ac44f8884994c0ec99
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-11-13 23:17:47 +0530

    Add ch3 francoid dl

---
+++ books/dl_FrancoisChollet/ch3/imdb_bin_classify.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tensorflow.keras.datasets import imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+
